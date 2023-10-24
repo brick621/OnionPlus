837ab8d8f0a053ce83e04ad4ad90c20c9a6ff5b4
Brick <brick621@proton.me>
2023-10-24 19:30:01 +0100

    Add "interact" plugin

---
+++ interact.py
@@ -0,0 +1,8 @@
+data = None
+
+def equip(gear: str = "Backpack") -> None: # default value "Backpack" is temporary
+    """Equip a piece of gear."""
+
+def load(savedata) -> None:
+    global data
+    data = savedata
