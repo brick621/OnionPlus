29bc7c413c5ef9be3888e2bb68d5db9020d17d07
Brick <brick621@proton.me>
2023-10-25 17:33:06 +0100

    Add quit command

--- misc.py
+++ misc.py
@@ -1,3 +1,4 @@
+import sys
 import colours
 
 data = None
@@ -6,6 +7,10 @@ def reload() -> None:
     """Reload the save file. (mainly used for debugging)"""
     data.load()
     print(f"{colours.OKGREEN}Data successfully reloaded!{colours.ENDC}")
+
+def quit() -> None:
+    """Exit the game :("""
+    sys.exit()
 
 def load(savedata) -> None:
     global data
