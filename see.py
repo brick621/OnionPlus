3834d520d993a95e1fca12e4d23f3f28967445c4
Brick <brick621@proton.me>
2023-10-29 15:50:24 +0000

    Add inventory command

--- see.py
+++ see.py
@@ -1,6 +1,7 @@
 """Commands for seeing your items and money etc."""
 
 import colours
+import utils
 
 data = None
 
@@ -12,6 +13,21 @@ def gear() -> None:
     """See what gear you currently have equiped."""
     for slot, value in data.gear.items():
         print(f"{colours.BOLD}{slot.capitalize()}: {colours.ENDC}{value}")
+
+def inventory(arg: str = "") -> None:
+    """See what is in your inventory.
+
+    Enter with argument "detailed" to see more info on the items.
+    """
+    for item in set(data.inventory):
+        print(str(data.inventory.count(item)) + " "
+              + colours.HEADER
+              + item
+              + colours.ENDC)
+        if arg == "detailed":
+            print(colours.OKBLUE
+                + utils.ITEMS[item]["description"]
+                + colours.ENDC)
 
 def load(savedata) -> None:
     global data
