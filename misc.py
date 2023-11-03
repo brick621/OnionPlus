e3d01fdc7d2fb95ccfd5eb7af2170fb9395029cf
Brick <brick621@proton.me>
2023-11-03 18:40:10 +0000

    Improve help commmand
    Instead of loading the entire modules into help, it store a dictionary with only important info.

--- misc.py
+++ misc.py
@@ -14,30 +14,29 @@ plugins = {}
 def help() -> None:
     """Describe what each command does"""
     print(f"{colours.HEADER}HELP{colours.ENDC}")
-    for plugin in plugins.values():
-        print(f"\n{colours.UNDERLINE}{plugin.__name__} -",
-              f"{inspect.getdoc(plugin)}{colours.ENDC}")
-        for name, value in inspect.getmembers(plugin, inspect.isfunction):
-            if name != "load":
-                fullargspec = inspect.getfullargspec(value)
-                args = fullargspec[0]
-                defaults = fullargspec[3] or ()
-                argslen = len(args)
-                if not args:
-                    # TODO: add support for multi line docstrings
-                    print(f"{colours.BOLD}• {name}:{colours.ENDC}",
-                          f"{inspect.getdoc(value)}")
-                else:
-                    # TODO: come up with a better name than "args_str_list"
-                    args_str_list = []
-                    for i, a in enumerate(args):
-                        if (argslen-i) <= len(defaults):
-                            args_str_list.append(f"[{a}: '{defaults[(argslen-i) - 1]}]'")
+    for p_name, plugin in plugins.items():
+        print(f"\n{colours.UNDERLINE}{p_name} -",
+              f"{plugin['_docstring']}{colours.ENDC}")
+        for f_name, value in plugin.items():
+            if f_name == "_docstring": continue
+            args = value["args"]
+            docstring = value["docstring"]
+            if not args:
+                print(f"{colours.BOLD}• {f_name}:{colours.ENDC}",
+                      f"{docstring}")
+            else:
+                args_str = ""
+                for arg, default in args:
+                    if default is not None:
+                        if default == "":
+                            args_str += f"[{arg}: ''] "
                         else:
-                            args_str_list.append(f"[{a}]")
-                    args = " ".join(args_str_list)
-                    print(f"{colours.BOLD}• {name} {args}:{colours.ENDC}",
-                          f"{inspect.getdoc(value)}")
+                            args_str += f"[{arg}: {default}] "
+                    else:
+                        args_str += f"[{arg}] "
+                args_str = args_str.strip()
+                print(f"{colours.BOLD}• {f_name} {args_str}:{colours.ENDC}",
+                      f"{docstring}")
 
 def reload() -> None:
     """Reload the save file. (mainly used for debugging)"""
@@ -51,4 +50,20 @@ def quit() -> None:
 def load(savedata, extensions) -> None:
     global data, plugins
     data = savedata
-    plugins = extensions
+    for plugin in extensions.values():
+        plugins[plugin.__name__] = {"_docstring": inspect.getdoc(plugin)}
+        for name, value in inspect.getmembers(plugin, inspect.isfunction):
+            if name != "load":
+                fullargspec = inspect.getfullargspec(value)
+                args = fullargspec[0]
+                defaults = fullargspec[3] or []
+                argslen = len(args)
+                plugins[plugin.__name__][name] = {
+                    "args": [],
+                    "docstring": inspect.getdoc(value)
+                }
+                if args:
+                    while len(defaults) < len(args):
+                        defaults.append(None)
+                    for arg, default in zip(reversed(args), defaults):
+                        plugins[plugin.__name__][name]["args"].insert(0, (arg, default))
