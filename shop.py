36127376eac759119550fd729e77f73e208ce495
Brick <brick621@proton.me>
2023-11-15 11:38:28 +0000

    Add sell command

--- shop.py
+++ shop.py
@@ -1,5 +1,8 @@
 """Commands for dealing with selling and buying items."""
 
+import colours
+import utils
+
 data = None
 
 
@@ -8,9 +11,32 @@ def sell(item: str, quantity: int = 1) -> None:
 
     Arguments:
     item -- the item you want to sell
-    quantity -- how much of the item
+    quantity -- the amount of items (any number higher than what you have will sell all).
     """
-    print(item, type(quantity))
+    global data
+
+    if item not in utils.ITEMS:
+        print(f"{colours.FAIL}That is not an item! Make sure to replace any spaces with hyphens ('-'){colours.ENDC}")
+        return
+    
+    if item not in data.inventory:
+        print(f"{colours.FAIL}You do not have that item")
+        return
+
+    if quantity <= 0:
+        print(f"{colours.FAIL}You have to have a positive quantity{colours.ENDC}")
+        return
+
+    if quantity > data.inventory.count(item):
+        quantity = data.inventory.count(item)
+
+    money_earned = utils.ITEMS[item]["sell-price"] * quantity
+    for _ in range(quantity):
+        data.inventory.remove(item)
+    data.money += money_earned
+    data.save()
+
+    print(f"{colours.OKGREEN}You earned {utils.money(money_earned, colours.OKBLUE)}{colours.OKGREEN}!{colours.ENDC}")
 
 
 def load(savedata) -> None:
